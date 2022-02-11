bc60c1af2cf6b03abee517f17f90052441f8d7e2
Cléa Aumont <56569375+cleaaum@users.noreply.github.com>
2022-02-11 09:31:24 -0500

    Add files via upload

---
+++ dictionnaries/country_avg_income.py
@@ -0,0 +1,27 @@
+income_dictionary = {
+    '1':          ['AFG', 'BFA', 'BDI', 'CAF', 'TCD', 'COD', 'ERI', 'ETH', 'GMB', 'GIN', 'GNB', 'PRK', 'LBR',
+                            'MDG', 'MWI', 'MLI', 'MOZ', 'NER', 'RWA', 'SLE', 'SOM', 'SSD', 'SDN', 'SYR', 'TGO', 'UGA',
+                            'YEM'],
+
+    '2': ['AGO', 'DZA', 'BGD', 'BLZ', 'BEN', 'BTN', 'BOL', 'CPV', 'KHM', 'CMR', 'COM', 'COG', 'CIV',
+                            'DJI', 'EGY', 'SLV', 'SWZ', 'GHA', 'HTI', 'HND', 'IND', 'IDN', 'IRN', 'KEN', 'KIR', 'KGZ',
+                            'LAO', 'LSO', 'MRT', 'FSM', 'MNG', 'MAR', 'MMR', 'NPL', 'NIC', 'NGA', 'PAK', 'PNG', 'PHL',
+                            'WSM', 'STP', 'SEN', 'SLB', 'LKA', 'TZA', 'TJK', 'TLS', 'TUN', 'UKR', 'UZB', 'VUT', 'VNM',
+                            'ZMB', 'ZWE', 'PSE'],
+
+    '3': ['ALB', 'ASM', 'ARG', 'ARM', 'AZE', 'BLR', 'BIH', 'BWA', 'BRA', 'BGR', 'CHN', 'COL', 'CRI',
+                            'CUB', 'DMA', 'DOM', 'GNQ', 'ECU', 'FJI', 'GAB', 'GEO', 'GRD', 'GTM', 'GUY', 'IRQ', 'JAM',
+                            'JOR', 'KAZ', 'LBN', 'LBY', 'MYS', 'MDV', 'MHL', 'MUS', 'MEX', 'MDA', 'MNE', 'NAM', 'MKD',
+                            'PAN', 'PRY', 'PER', 'ROU', 'RUS', 'SRB', 'ZAF', 'LCA', 'VCT', 'SUR', 'THA', 'TON', 'TUR',
+                            'TKM', 'TUV'],
+
+    '4':         ['AND', 'AIA', 'ALA', 'ATG', 'ABW', 'AUS', 'AUT', 'BES', 'BHS', 'BHR', 'BRB', 'BEL', 'BMU',
+                            'VGB', 'BRN', 'CAN', 'CYM', 'CHL', 'HRV', 'CUW', 'CYP', 'CZE', 'DNK', 'EST', 'FIN', 'CYP',
+                            'CZE', 'DNK', 'EST', 'FRO', 'FIN', 'FRA', 'PYF', 'DEU', 'GGY', 'GIB', 'GLP', 'GRC', 'GRL',
+                            'GUM', 'HKG', 'HUN', 'ISL', 'IRL', 'IMN', 'ISR', 'ITA', 'JPN', 'KOR', 'KWT', 'LVA', 'LIE',
+                            'LTU', 'LUX', 'MAC', 'MCO', 'MLT', 'MTQ', 'NRU', 'NLD', 'NCL', 'NZL', 'MNP', 'NOR', 'OMN',
+                            'PLW', 'POL', 'PRT', 'PRI', 'QAT', 'REU', 'SMR', 'SAU', 'SYC', 'SGP', 'SXM', 'SVK', 'SVN',
+                            'ESP', 'KNA', 'MAF', 'SWE', 'CHE', 'TWN', 'TTO', 'TCA', 'ARE', 'GBR', 'USA', 'URY', 'VIR',
+                            'VEN']
+}
+
